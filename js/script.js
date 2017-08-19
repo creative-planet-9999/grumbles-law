36bce610ad5e24e68d672d89c26fb33284e1bd9e
Zubayer <haider15-5902@diu.edu.bd>
2017-08-19 21:55:37 +0600

    Animation + Structure Done

--- js/script.js
+++ js/script.js
@@ -10,3 +10,12 @@ function toggleMenuDesktop (){
     [].forEach.call(document.querySelectorAll('.header-menu'), function(el){ el.classList.toggle('hidden'); });
     elem.removeEventListener('touchstart', toggleMenu);
 }
+
+
+var intervalID = setInterval(function(){
+    // every 4 seconds execute following
+    var visibleWord = document.getElementsByClassName('elem-visible')[0],
+        hiddenWord = document.getElementsByClassName('elem-hidden')[0];
+    visibleWord.setAttribute('class','elem-hidden');
+    hiddenWord.setAttribute('class','elem-visible');
+}, 10000);
