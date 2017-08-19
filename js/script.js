c65b0585949cd2b4e35330071389b8854e8802c8
Zubayer <haider15-5902@diu.edu.bd>
2017-08-19 17:48:54 +0600

    Restructure

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-document.getElementById("navbar-btn").addEventListener("click", function(event){
-    event.preventDefault();
-    var target = document.getElementById("nav").classList;
-    if ( target.contains("open") ) {
-        target.remove("open"); }
-    else {
-        target.add("open"); }
-});
+var elem = document.getElementById('hamburger-menu');
+elem.addEventListener('touchstart', toggleMenu);
+elem.addEventListener('click', toggleMenuDesktop);
+
+function toggleMenu (){
+    [].forEach.call(document.querySelectorAll('.header-menu'), function(el){ el.classList.toggle('hidden'); });
+    elem.removeEventListener('click', toggleMenuDesktop);
+}
+function toggleMenuDesktop (){
+    [].forEach.call(document.querySelectorAll('.header-menu'), function(el){ el.classList.toggle('hidden'); });
+    elem.removeEventListener('touchstart', toggleMenu);
+}
